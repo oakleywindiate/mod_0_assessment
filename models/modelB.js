c2dd05ecb67d923c60921a1ccc67ee5d3d948368
Oakley Windiate <oakley.windiate@gmail.com>
2021-11-12 19:42:08 -0700

    Add house model to program specific challenge

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var houseObject = {
+  exteriorFinish: ["wood", "brick", "rock"],
+  yearsOld: 18,
+  isSold: false,
+  hasPool: true,
+  numberOfTrees: 4,
+  furniture: ["couch", "desk", "table"],
+  streetName: "Lost Creek",
+  paintColor: "blue"
+};
