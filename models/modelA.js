b36021938f02dc26edb8341b6946268c6617040e
Oakley Windiate <oakley.windiate@gmail.com>
2021-11-12 19:31:59 -0700

    Add laptop model to program specific challenge

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var laptopModel = {
+  color: "grey",
+  modelNumber: 13,
+  hasCharger: true,
+  currentSoftware: true,
+  installedApps: ["Word", "Excel", "Powerpoint"],
+  accessories: ["case", "stand", "screen protector"],
+  weightInLbs: 4,
+  defects: "Scratch on screen"
+};
