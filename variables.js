acbd17062f130d3d543b2ee04751a82475fee7f8
Oakley Windiate <oakley.windiate@gmail.com>
2021-11-12 19:11:50 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var animal = "Zebra";
+var isCuddly = true;
+animal = "Lion";
+var num8 = 8;
+var num3 = 3;
+var quotient = (num3 / num8);
+var favoriteFilms = ["Dune", "Interstellar", "The Green Knight" ];
+favoriteFilms.shift();
+console.log(animal.length);
